4beadc752de821ec8d0e7c91bdb77f5517640c8e
Anton-Astrakhantsev <36815626+Anton-Astrakhantsev@users.noreply.github.com>
2019-04-23 16:10:39 +0300

    Create N3.sql

---
+++ P2_SQL/HomeWork/N3.sql
@@ -0,0 +1,21 @@
+
+-- Выведите магазины, имеющие больше 300-от покупателей
+select
+	store_id,
+	count(distinct customer_id)
+from
+	rental
+	left join staff using (staff_id)
+group by store_id
+having
+	count(distinct customer_id) > 300;
+
+
+-- Выведите у каждого покупателя город в котором он живет
+select
+	customer_id,
+	city
+from
+	customer as cus
+	left join address as adr on(cus.address_id=adr.address_id)
+	left join city using(city_id)
