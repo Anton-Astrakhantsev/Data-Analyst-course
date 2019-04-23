2f983ad1ab03187293be949c2dc9f52e7dd4a9ce
Anton-Astrakhantsev <36815626+Anton-Astrakhantsev@users.noreply.github.com>
2019-04-23 15:38:38 +0300

    Create N2.sql

---
+++ P2_SQL/HomeWork/N2.sql
@@ -0,0 +1,18 @@
+-- Вывести всех неактивных покупателей
+select
+	*
+from
+	public.customer
+where
+	active = 0;
+
+
+--Вывести 10 последних платежей за прокат фильмов
+select
+	*
+from
+	public.payment
+order by
+	payment_date desc,
+	payment_id desc
+limit 20;
