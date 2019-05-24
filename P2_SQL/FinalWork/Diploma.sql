3b82d49d66cfa07ebff9fe69cf1722ea58433a44
Anton-Astrakhantsev <36815626+Anton-Astrakhantsev@users.noreply.github.com>
2019-05-24 12:13:26 +0300

    Update Diploma.sql

--- P2_SQL/FinalWork/Diploma.sql
+++ P2_SQL/FinalWork/Diploma.sql
@@ -6,7 +6,8 @@ from
 	bookings.airports
 group by city
 having
-	count(airport_code) > 1
+	count(airport_code) > 1;
+
 
 -- 2. Были ли брони, по которым не совершались перелеты?
 select
@@ -17,9 +18,10 @@ from
 	right join bookings.ticket_flights using (ticket_no)
 	left join bookings.flights using (flight_id)
 where
-	actual_arrival is null
+	actual_arrival is null;
 
--- В каких аэропортах есть рейсы, которые обслуживаются самолетами с максимальной дальностью перелетов?
+
+-- 3. В каких аэропортах есть рейсы, которые обслуживаются самолетами с максимальной дальностью перелетов?
 with max_range_plane as (
 select
 	aircraft_code
@@ -47,9 +49,10 @@ select
 	distinct airport_name
 from
 	max_plane_airports
-	left join bookings.airports using (airport_code)
+	left join bookings.airports using (airport_code);
 
--- Между какими городами пассажиры делали пересадки?
+
+-- 4. Между какими городами пассажиры делали пересадки?
 with ways as (
 select
 	ticket_no,
@@ -76,4 +79,39 @@ select
 from
 	ways
 where
+	ticket_no in (select ticket_no from transfer);
+
+-- 4.1 При полетах между какими городами делают пересадки чаще?
+with ways as (
+select
+	ticket_no,
+	da.city as dep_city,
+	aa.city as arr_city,
+	first_value(da.city) over (partition by ticket_no order by actual_departure asc)
+	|| ' - ' ||
+	first_value(aa.city) over (partition by ticket_no order by actual_departure desc) as route
+from
+	bookings.ticket_flights
+	left join bookings.flights as f using (flight_id)
+	left join bookings.airports as da on(f.departure_airport=da.airport_code)
+	left join bookings.airports as aa on(f.arrival_airport=aa.airport_code)
+order by ticket_no asc, actual_departure asc
+),
+transfer as (
+select ticket_no
+from ways
+group by ticket_no
+having count(ticket_no) > 1
+)
+select
+	route,
+	count(distinct ticket_no) as transfer_count
+from
+	ways
+where
 	ticket_no in (select ticket_no from transfer)
+group by route
+order by transfer_count desc
+limit 5;
+
+
